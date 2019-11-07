185c748f43b711b5b0b3b86a69546ee29d60f706
Ars Golushkov <arsen.gl@yandex.ru>
2019-11-07 15:10:24 +0300

    events is home page

--- src/epic/utils.js
+++ src/epic/utils.js
@@ -3,5 +3,5 @@
 const validEpicViewIds: EpicViewId[] = ['main', 'menu', 'events', 'check-params'];
 
 export function extractEpicViewId(src: ?string):EpicViewId {
-  return validEpicViewIds.find(id => id === src) || 'main'
+  return validEpicViewIds.find(id => id === src) || 'events'
 }
